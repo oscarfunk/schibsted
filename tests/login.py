c40f6a61a28741acdc4ef949d02a4c433368dbce
unknown <digital.oz@gmail.com>
2016-06-01 10:08:23 -0400

    fix issue with login suite

--- tests/login.py
+++ tests/login.py
@@ -25,44 +25,45 @@ class login(unittest.TestCase):
         assert Login_Page.is_successful_login, "User or password is not valid."
         self.driver.save_screenshot('C:\\schibsted\\results\\screenshots\\test_HappyPath.png')
 
-#     def test_Invalid_Password(self):
-#         login = page.LoginPage(self.driver)
-#         login.enter_user('test')
-#         login.enter_password('123126')
-#         login.click_submit_button()
-#         Login_Page = page.LoginPage(self.driver)
-#         assert Login_Page.is_failed_login
+    def test_Invalid_Password(self):
+        login = page.LoginPage(self.driver)
+        login.enter_user('test')
+        login.enter_password('123126')
+        login.click_submit_button()
+        Login_Page = page.LoginPage(self.driver)
+        assert Login_Page.is_failed_login
+        self.driver.save_screenshot('C:\\schibsted\\results\\screenshots\\test_Invalid_Password.png')
 
-#     def test_Invalid_User(self):
-#         login = page.LoginPage(self.driver)
-#         login.enter_user('invalid')
-#         login.enter_password('123126')
-#         login.click_submit_button()
-#         Login_Page = page.LoginPage(self.driver)
-#         assert Login_Page.is_failed_login
+    def test_Invalid_User(self):
+        login = page.LoginPage(self.driver)
+        login.enter_user('invalid')
+        login.enter_password('123126')
+        login.click_submit_button()
+        Login_Page = page.LoginPage(self.driver)
+        assert Login_Page.is_failed_login
 
-#     def test_Valid_User_no_Password(self):
-#         login = page.LoginPage(self.driver)
-#         login.enter_user('test')
-#         login.click_submit_button()
-#         Login_Page = page.LoginPage(self.driver)
-#         assert Login_Page.is_failed_login
+    def test_Valid_User_no_Password(self):
+        login = page.LoginPage(self.driver)
+        login.enter_user('test')
+        login.click_submit_button()
+        Login_Page = page.LoginPage(self.driver)
+        assert Login_Page.is_failed_login
 
-#     def test_No_User(self):
-#         login = page.LoginPage(self.driver)
-#         login.enter_password('123456')
-#         login.click_submit_button()
-#         Login_Page = page.LoginPage(self.driver)
-#         assert Login_Page.is_failed_login
+    def test_No_User(self):
+        login = page.LoginPage(self.driver)
+        login.enter_password('123456')
+        login.click_submit_button()
+        Login_Page = page.LoginPage(self.driver)
+        assert Login_Page.is_failed_login
 
-#     def test_No_Input(self):
-#         login = page.LoginPage(self.driver)
-#         login.click_submit_button()
-#         Login_Page = page.LoginPage(self.driver)
-#         assert Login_Page.is_failed_login
+    def test_No_Input(self):
+        login = page.LoginPage(self.driver)
+        login.click_submit_button()
+        Login_Page = page.LoginPage(self.driver)
+        assert Login_Page.is_failed_login
 
-#     def tearDown(self):
-#          self.driver.close()
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
      unittest.main()
